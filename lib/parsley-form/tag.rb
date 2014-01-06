05d39adf642099202b61bc2c71562906b991de17
Chris Ell <cell@weblinc.com>
2014-01-06 10:56:54 -0500

    Adds comments to Tag model

--- lib/parsley-form/tag.rb
+++ lib/parsley-form/tag.rb
@@ -10,21 +10,25 @@ module ValidationFormHelper
 
     def initialize(model_attribute, validators, options = {})
       @model_attribute, @validators, @options = model_attribute, validators, options
-      additions
+      add_markup_validation
     end
 
-    def additions
+    #
+    # Loops through validations passed to the Tag, collects them into a hash, then
+    # merges into the @options variable.
+    #
+    def add_markup_validation
       validations = {}
       @validators.each do |validation|
-        case validation[:type]
+        validations.merge! case validation[:type]
           when ActiveModel::Validations::PresenceValidator
-            validations.merge! validate_presence(validation)
+             validate_presence(validation)
           when ActiveModel::Validations::NumericalityValidator
-            validations.merge! validate_numericality(validation)
+            validate_numericality(validation)
           when ActiveModel::Validations::LengthValidator
-            validations.merge! validate_length(validation)
+            validate_length(validation)
           when ActiveModel::Validations::FormatValidator
-            validations.merge! validate_format(validation)
+            validate_format(validation)
           else
             ''
           end
@@ -33,6 +37,13 @@ module ValidationFormHelper
     end
 
   private
+
+  #
+  # Validates length of an attribute
+  #
+  # @param [ActiveModel::Validations::LengthValidator]
+  # @return [Hash] HTML option for tag with validation
+  #
     def validate_length(validation)
       addition = case validation[:options].length
       when 1
@@ -49,6 +60,12 @@ module ValidationFormHelper
       add_message_for_validation(addition, validation)
     end
 
+  #
+  # Validates numericality of an attribute
+  #
+  # @param [ActiveModel::Validations::NumericalityValidator]
+  # @return [Hash] HTML option for tag with validation
+  #
     def validate_numericality(validation)
       addition = if validation[:options][:only_integer]
                   { 'parsley-type' => 'digits' }
@@ -58,16 +75,35 @@ module ValidationFormHelper
       add_message_for_validation(addition, validation)
     end
 
+  #
+  # Validates presence of an attribute
+  #
+  # @param [ActiveModel::Validations::PresenceValidator]
+  # @return [Hash] HTML option for tag with validation
+  #
     def validate_presence(validation)
       addition = { 'parsley-required' => "true" }
       add_message_for_validation(addition, validation)
     end
 
+  #
+  # Validates format of an attribute
+  #
+  # @param [ActiveModel::Validations::FormatValidator]
+  # @return [Hash] HTML option for tag with validation
+  #
     def validate_format(validation)
       addtion = { 'parsley-regexp' => validation[:options][:with] } if validation[:options][:with].present?
       add_message_for_validation(addition, validation)
     end
 
+  #
+  # Adds an optional validation message if it exists in the options of the model's validation.
+  #
+  # @param [Hash] HTML option for tag with validation
+  # @param [ActiveModel::Validations] Some subclass of the Validations class.
+  # @return [Hash] HTML option for tag with validation with message
+  #
     def add_message_for_validation(addition, validation)
         addition.merge!({ 'parsley-error-message' => validation[:options][:message] }) if validation[:options][:message].present?
         addition
